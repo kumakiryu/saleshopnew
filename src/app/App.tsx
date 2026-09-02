7b7496e81b51e57179b488dace8c0ecd95e3b08f
kumakiryu <formybusinessdesu@gmail.com>
2026-09-02 21:31:47 +0800

    Refactor App component to include MaintenanceGate
    Removed QueryClientProvider and added MaintenanceGate for routing.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import MaintenanceGate from './components/MaintenanceGate';
 import ShopPage from './pages/ShopPage';
 import AdminPage from './pages/AdminPage';
 import AnnouncementsPage from './pages/AnnouncementsPage';
@@ -7,23 +7,21 @@ import CartPage from './pages/CartPage';
 import CheckoutPage from './pages/CheckoutPage';
 import OrderStatusPage from './pages/OrderStatusPage';
 
-const queryClient = new QueryClient();
-
 export default function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<ShopPage />} />
-          <Route path="/stock" element={<ShopPage />} />
-          <Route path="/announcements" element={<AnnouncementsPage />} />
-          <Route path="/cart" element={<CartPage />} />
-          <Route path="/checkout" element={<CheckoutPage />} />
-          <Route path="/order-status/:id" element={<OrderStatusPage />} />
-          <Route path="/admin" element={<AdminPage />} />
-          <Route path="*" element={<ShopPage />} />
-        </Routes>
-      </BrowserRouter>
-    </QueryClientProvider>
+    <BrowserRouter>
+      <MaintenanceGate>
+      <Routes>
+        <Route path="/" element={<ShopPage />} />
+        <Route path="/stock" element={<ShopPage />} />
+        <Route path="/announcements" element={<AnnouncementsPage />} />
+        <Route path="/cart" element={<CartPage />} />
+        <Route path="/checkout" element={<CheckoutPage />} />
+        <Route path="/order-status/:id" element={<OrderStatusPage />} />
+        <Route path="/admin" element={<AdminPage />} />
+        <Route path="*" element={<ShopPage />} />
+      </Routes>
+      </MaintenanceGate>
+    </BrowserRouter>
   );
 }
